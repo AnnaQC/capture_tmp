79ad0ee7d9ef6450240388d69a208b16ff6013dc
AnnaQC <AnnaQC@github.com>
2016-06-24 14:46:37 +0300

    Revert "Added twitter tests. Small bug Fixes"
    This reverts commit f6d3954218efae1f4ae08bc1974cf18d49214a69.

--- patterns/core/pattern_handler.py
+++ patterns/core/pattern_handler.py
@@ -10,7 +10,7 @@ from selenium.common.exceptions import WebDriverException
 import allure
 
 
-def get_actual_data_from_js_console(driver, time_to_wait=3):
+def get_actual_data_from_js_console(driver):
     """
     Gets captured data from pattern and erases unused data attributes
 
@@ -18,31 +18,10 @@ def get_actual_data_from_js_console(driver, time_to_wait=3):
     :return: actual_res: dictionary of captured data without attributes which names contain 'c' and '_'
     """
     driver.current_url
-    actual_res = None
+    wd_log = driver.get_log('browser')
     print "**"*10 + "Data from captured results by pattern" + "**"*10
-    actual_res = read_capture_result_message_from_js(driver.get_log('browser'))
-    if actual_res is None:
-        time.sleep(time_to_wait)
-        driver.current_url
-        actual_res = read_capture_result_message_from_js(driver.get_log('browser'))
-        if actual_res is None:
-            raise WebDriverException("Captured results were not found within the WEB DRIVER log (browser console) \n.\
-            Check workabillity of selection.js of extension and debug driver.get_log('browser') or restart test.")
-    return actual_res
-
-
-def handle_value_to_print(value):
-    """ change type of attribute values to string """
-    if value is None:
-        value = 'None'
-    elif isinstance(value, unicode):
-         if value is u'':
-             value = 'None'
-         value = value.encode("utf-8")
-    return value.replace('. ', '.')
-
-def read_capture_result_message_from_js(wd_log):
     for entry in wd_log:
+
         if '{"c"' in entry['message']:
             msg = entry['message']
             msg = msg[msg.find("{"):msg.rfind("}") + 1].encode('utf-8')
@@ -61,4 +40,16 @@ def read_capture_result_message_from_js(wd_log):
                 allure.attach(value[0], json.dumps(value[1]))
             print "***" * 20
             return actual_res
-    return None
+    raise WebDriverException("Captured results were not found within the WEB DRIVER log (browser console) \n.\
+    Check workabillity of selection.js of extension and debug driver.get_log('browser').")
+
+
+def handle_value_to_print(value):
+    """ change type of attribute values to string """
+    if value is None:
+        value = 'None'
+    elif isinstance(value, unicode):
+         if value is u'':
+             value = 'None'
+         value = value.encode("utf-8")
+    return value.replace('. ', '.')
