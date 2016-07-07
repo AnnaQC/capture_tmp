6d40cd31a420675d37ef3d701fbd330abca2711e
annaQC <zagann.si@gmail.com>
2016-07-07 13:59:33 +0300

    Update test_linkedin_pattern.py

--- patterns/tests/test_linkedin_pattern.py
+++ patterns/tests/test_linkedin_pattern.py
@@ -26,7 +26,9 @@ def test_profile_ptrn_for_logged_in_user(chromedriver, test_page):
     test_page = '..' + config.linkedin_test_data + test_page
     #linkidin = linkidin_profile.LinkidinProfilePage(chromedriver)
     linkidin_auth = authorization.LinkidinAuthPage(chromedriver, test_page, 'project')
+    time.sleep(3)
     linkidin_auth.login(linkidin_auth.sign_in_link, linkidin_auth.login_form, linkidin_auth.default_user)
+    time.sleep(3)
     linkidin_auth.open(config.read_options_for('linkedin', test_page))
     time.sleep(3)
     with allure.step('Read expected result from profile page'):
